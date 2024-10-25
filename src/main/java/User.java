3c8e25c1892a975e43d259df798ccd648a639f6a
Valere Nz. <nz-valere>
2024-10-25 11:16:25 +0100

    user class

---
+++ src/main/java/User.java
@@ -0,0 +1,11 @@
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    private String email;
+    private String phone;
+    private String city;
+    private double balancePersonnel;
+
+    // Constructors, Getters, and Setters
+}
